724fda866d3b7da9e9ecc85fe07ca1a700352f30
Anna Zach <anza2501@student.miun.se>
2026-03-05 12:22:49 +0100

    Uppdatera Vite-konfig för att inkludera animation.html

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: 'index.html',
-        sass: 'sass.html'
+        sass: 'sass.html', 
+        animation: 'animation.html'
       }
     }
   }
